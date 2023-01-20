1f1ddfba7f2641a0d1cde4161ee076c5aba4a4bc
Marzena-W <marzena.m.wilk@gmail.com>
2023-01-20 23:44:51 +0000

    created current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,24 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-var time = moment();
-// $("#currentTime").text(time.format('LTS'));
-$("#currentTime").text(time.format('LTS'));
-
-var time = moment().format("HH:mm:ss");
-$("#4a").text(time);
-
+// Display the current day
+var currentTime = moment();
+$("#currentTime").text(currentTime.format('LT'));
 
 
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+var color = document.getElementsByClassName("hour");
+var date = new Date();
+var currentHour = date.getHours();
+console.log(currentHour);
+
+
+
+
+
 
 // Save the event in local storage when the save button is clicked in that timeblock.
+
+
 
 // Persist events between refreshes of a page
