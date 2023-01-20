f9b5079c69d1387914906c5e2a4fb2e7f1d0ffb2
Marzena-W <marzena.m.wilk@gmail.com>
2023-01-20 23:04:33 +0000

    changes to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-// current date using Moments.js
+// Display the current day
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+var time = moment();
+// $("#currentTime").text(time.format('LTS'));
+$("#currentTime").text(time.format('LTS'));
+
+var time = moment().format("HH:mm:ss");
+$("#4a").text(time);
+
+
+
+// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+
+// Save the event in local storage when the save button is clicked in that timeblock.
+
+// Persist events between refreshes of a page
