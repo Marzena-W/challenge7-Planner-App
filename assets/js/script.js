5430fdfc0218df6c3cfa9effcbdae4ccbb6937d2
Marzena-W <marzena.m.wilk@gmail.com>
2023-01-21 23:43:38 +0000

    added function to save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ console.log(currentHour);
 
 
 
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+// Color-code each timeblock based on past, present, and future
 function checkTime() {
     for (var i = 9; i < 18; i++) { // workday starts at 9 and finishes at 17
         var textArea = document.getElementById("userMemo" + i) // to select textarea by id + i
@@ -23,6 +23,29 @@ function checkTime() {
             currentHour > i
             textArea.classList.add("future");
         }
+        showMemos(i);
     }
 }
 checkTime();
+
+
+// Save the event in local storage when the save button is clicked in that timebloc
+var index = 9;
+var saveBtn = document.querySelectorAll(".saveBtn");
+for (var i = 0; i < saveBtn.length; i++) {
+    saveBtn[i].addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log(index)
+
+        var savedMemo = document.querySelector("#userMemo" + index).value;
+        localStorage.setItem("savedMemo" + index, savedMemo);
+        index++;
+    })
+};
+
+var userMemo = document.querySelector("#userMemo");
+function showMemos(i) {
+    var savedMemo = localStorage.getItem("savedMemo" + i);
+    var textArea = document.getElementById("userMemo" + i);
+    textArea.value = savedMemo;
+}
